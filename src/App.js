bd7fedb9e3aaba42502f3e72cb1b1095aceda8d0
Nilupul Heshan <niluheshan@gmail.com>
2020-11-07 15:45:47 +0530

    latest - 2020-11-05

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/navBar";
+import Home from "./mainPages/home";
+import Player from "./mainPages/player";
+import AdminDashboard from "./admin/adminDash";
+import NotFound from "./mainPages/notFound";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Switch>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/player" component={Player}></Route>
+        <Route path="/admin" component={AdminDashboard}></Route>
+        <Route path="/notFound" component={NotFound}></Route>
+        <Redirect from="/" exact to="/home"></Redirect>
+        <Redirect to="/notFound"></Redirect>
+      </Switch>
     </div>
   );
 }
